8293cfafa38a1fccd6c11fea2905dd4f2193b58b
Matt Jennings <matt@mattjennings.net>
2014-10-10 16:39:51 -0700

    Able to wrap HTML tags around value in form

--- js/local.js
+++ js/local.js
@@ -1,7 +1,18 @@
 $(function() {
 
 
+    var textAddTest = "";
+
+    $("#generate-email").on("click", function(event) {
+
+        event.preventDefault();
+        //$("textarea").append("Blah");
+
+        var textAddTest = $("#text-add-test").val();
+
+        $("textarea").append("&lt;html&gt;" + textAddTest + "&lt;/html&gt;");
 
 
+    });
 
 });
