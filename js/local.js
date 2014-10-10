a1f6a070e159af36def5d380ef1616b006569126
Matt Jennings <matt@mattjennings.net>
2014-10-10 16:52:40 -0700

    Basic HTML email generator done

--- js/local.js
+++ js/local.js
@@ -4,14 +4,17 @@ $(function() {
     var textAddTest = "";
 
     $("#generate-email").on("click", function(event) {
-
         event.preventDefault();
-        //$("textarea").append("Blah");
 
         var textAddTest = $("#text-add-test").val();
-
         $("textarea").append("&lt;html&gt;" + textAddTest + "&lt;/html&gt;");
 
+    });
+
+    $("#reset").on("click", function(event) {
+        event.preventDefault();
+        $("textarea").html("");
+        $("input").val("");
 
     });
 
